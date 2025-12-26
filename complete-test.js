f123e834f361bdd5543b138d42a7cfe15ae01cc1
kuzn3tsov <lem.treursic@gmail.com>
2025-12-27 00:18:10 +0100

    fixing vercel frontend

--- complete-test.js
+++ complete-test.js
@@ -84,7 +84,7 @@ async function runCompleteTestSuite() {
             const controller = new AbortController();
             const timeoutId = setTimeout(() => controller.abort(), 30000);
 
-            const response = await fetch('http://localhost:3000/api/screenshot', {
+            const response = await fetch('https://image-lemgendizer-old-x2qz.vercel.app/api/screenshot', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
